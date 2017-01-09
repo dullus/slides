be3529a70b6453bc8f2729307ace53912ad58c4d
Michal Vanko <mklakakilli@gmail.com>
2017-01-09 17:17:12 +0100

    WIP Implementation of guess the number

---
+++ guess-the-number.js
@@ -0,0 +1,11 @@
+// Overime si, ci sa subor nacital
+console.log('Subor guess-the-number.js sa nacital');
+
+// Vygenerujeme nahodne cislo od 0 - 100
+
+// Math.random() vygeneruje cislo od 0 - 1
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+let randomNumber = Math.random();
+
+// 
+let secretNumber
